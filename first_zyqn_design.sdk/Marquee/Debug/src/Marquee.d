176bbbffa86af22d61aa8b4ed94d981e652556e6
persueadream <graduation201209@163.com>
2017-08-31 23:04:16 +0800

    Initial commit

---
+++ first_zyqn_design.sdk/Marquee/Debug/src/Marquee.d
@@ -0,0 +1,46 @@
+src/Marquee.o src/Marquee.d: ../src/Marquee.c \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Marquee_bsp/ps7_cortexa9_0/include/xil_cache.h
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Marquee_bsp/ps7_cortexa9_0/include/xil_cache.h:
---
+++ first_zyqn_design.sdk/Marquee/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LD_SRCS += \
+../src/lscript.ld 
+
+C_SRCS += \
+../src/Marquee.c 
+
+OBJS += \
+./src/Marquee.o 
+
+C_DEPS += \
+./src/Marquee.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM gcc compiler'
+	arm-xilinx-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -I../../Marquee_bsp/ps7_cortexa9_0/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
